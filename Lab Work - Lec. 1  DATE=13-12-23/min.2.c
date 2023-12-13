dffec57b8309ab9c27acddcdb1f16d7ce55259c3
Rajpatel6331 <152473915+Rajpatel6331@users.noreply.github.com>
2023-12-13 12:57:21 +0530

    Add files via upload

---
+++ Lab Work - Lec. 1  DATE=13-12-23/min.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+    int main(){
+        int a,b;
+        printf("Enter your min no");
+        scanf("%d %d",&a,&b);
+
+        if (a<b){
+            printf("a is  min");
+        }else{
+             printf("b is  min");
+        }
+ 
+        return 0;
+    }
+    
