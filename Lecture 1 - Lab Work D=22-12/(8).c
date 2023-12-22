635190f73d931cb636bb20c81aff2fdcfcca7966
Rajpatel6331 <152473915+Rajpatel6331@users.noreply.github.com>
2023-12-22 13:33:28 +0530

    Add files via upload

---
+++ Lecture 1 - Lab Work D=22-12/(8).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+    int i, j,l=1;
+    for (i=1;i<=5;i++){
+        for (j=1;j<=i;j++){      
+        printf(" %d",l);
+        l++;
+        }
+                printf("\n");
+
+    }
+
+    return 0;
+}
